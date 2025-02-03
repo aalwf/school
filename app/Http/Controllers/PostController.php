be236c964cfa0e1b0d2d2e9577d3315fb8bb76ea
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-03 21:58:10 +0700

    import Redirect Type from Laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse; // Import redirect from Illuminate Laravel
 use Illuminate\View\View; // Import view from Illuminate Laravel
 use App\Models\Post; // Import model Post
 
