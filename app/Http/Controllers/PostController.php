61e37447fbcea328ca0489729c2c5503d0419b02
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-03 21:03:18 +0700

    import View Type from Laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View; // Import view from Illuminate Laravel
 use App\Models\Post; // Import model Post
 
 class PostController extends Controller
