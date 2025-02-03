c529d32e85d82c1967848ca081813f4037045c97
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-03 23:18:29 +0700

    send data to view

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,6 +18,6 @@ class ProductController extends Controller
         ];
 
         // Menampilkan halaman postingan
-        return view('pages.index', compact('products'));
+        return view('pages.index', $data);
     }
 }
