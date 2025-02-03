f295d6c7e4f6d7aac72055a573411dba0bafb3df
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-03 11:37:49 +0700

    js cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,15 @@
         @yield('content') {{-- menampilkan konten --}}
     </div>
 
+    {{-- Import jQuery CDN --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    {{-- Import Bootstrap JS CDN --}}
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    {{-- Import toastr JS CDN --}}
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
 </body>
 
 </html>
