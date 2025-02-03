e09e69e7202698d60619b58ef3def666e685eb90
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-03 11:40:23 +0700

    toastr setup in posts view

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -56,4 +56,15 @@
             </div>
         </div>
     </div>
+
+    <script>
+        // menampilkan pesan toastr dari session
+        @if (session()->has('success'))
+
+            toastr.success('{{ session('success') }}', 'BERHASIL!');
+        @elseif (session()->has('error'))
+
+            toastr.error('{{ session('error') }}', 'GAGAL!');
+        @endif
+    </script>
 @endsection
