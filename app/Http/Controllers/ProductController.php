823a786d8b836ff482c4a5e930dd2c8be7dc0ca6
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-03 23:29:15 +0700

    create method for store data product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -33,4 +33,36 @@ class ProductController extends Controller
         // Menampilkan halaman postingan
         return view('pages.create', $data);
     }
+
+    // Method untuk menyimpan produk
+    public function store(Request $request): RedirectResponse
+    {
+        // validasi data yang dikirim dari form
+        $request->validate([
+            'image'         => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+
+        // mengambil file gambar dari form yang dikirim
+        $image = $request->file('image');
+
+        // menggunakan method storeAs untuk menyimpan gambar ke folder posts
+        $image->storeAs('public/products', $image->hashName());
+
+        // menambahkan data ke database
+        Product::create([
+            'image'         => $image->hashName(),
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'price'         => $request->price,
+            'stock'         => $request->stock
+        ]);
+
+        // mengalihkan ke halaman posts dengan mengirimkan pesan sukses
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
 }
