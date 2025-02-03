c145a267706f51bf8f0aea6ce8294864faf2b774
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-03 21:05:11 +0700

    create index method in PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,5 +8,13 @@ use App\Models\Post; // Import model Post
 
 class PostController extends Controller
 {
-    //
+    // Method untuk menampilkan postingan
+    public function index()
+    {
+        // Mengambil semua data postingan dengan mengurutkan postingan terbaru & melimit 5 data per halaman
+        $posts = Post::latest()->paginate(5);
+
+        // Menampilkan halaman postingan
+        return view('posts.index', compact('posts'));
+    }
 }
