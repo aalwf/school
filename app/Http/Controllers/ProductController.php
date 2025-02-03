974065609f7e2f52a8e37ff29766413e242becc2
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-03 23:03:15 +0700

    import type view dan model in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View; // View dari Laravel
+use App\Models\Product; // Model Product
 
 class ProductController extends Controller
 {
