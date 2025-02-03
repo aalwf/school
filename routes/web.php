db2ecfe43a99d0fb784b2ac1d0ac04c772c80a1c
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-03 23:17:00 +0700

    create route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController; // Import ProductController
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Jalur untuk resource product
+Route::resource('/products', \App\Http\Controllers\ProductController::class);
