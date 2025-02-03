9fb51edcea423ec8b11932f7520bfc2af5c82a7f
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-03 11:18:17 +0700

    create resource route for post in route

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Jalur Resource untuk Postingan
+Route::resource('posts', PostController::class);
