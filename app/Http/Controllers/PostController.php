3444b5154e6234b7a9f56cc195aae71e1bfa30ae
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-03 11:05:26 +0700

    get all posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,6 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        // Get all posts from the Post model and limit the data displayed to 5 on each page
+        $posts = Post::latest()->paginate(5);
     }
 }
