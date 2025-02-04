b21de542c8aa8d48cd6f4fde3259d69bcfa89673
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-04 22:23:05 +0700

    determine post fillable field in model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // Field yang dapat diisi
+    protected $fillable = [
+        'image',
+        'title',
+        'content',
+    ];
 }
