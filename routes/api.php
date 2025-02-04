f8368bb0fbee5c72aff583dfca3cc8413181acce
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-04 22:33:13 +0700

    add new route for crud needs

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,6 @@ use App\Http\Controllers\Api\PostController; // import controller Post
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Route Resource untuk Post
+Route::apiResource('posts', PostController::class);
