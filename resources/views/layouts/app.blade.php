55718b1015884a277d194938d3757c3aa7ac3d90
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-04 12:01:03 +0700

    sweetalert fix error cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
     {{-- SweetAlert CDN --}}
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
     {{-- CKEditor CDN --}}
     <script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
