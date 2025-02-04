3a1a17fb192a2052e14488749843439ed9811fbc
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-04 21:40:13 +0700

    import validator in controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator; // import validator dari laravel
 use App\Http\Resources\PostResource; // import resource Post
 use App\Models\Post; // import model Post
 
