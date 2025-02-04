802a5aa8f0942f2b14cc098167ace02af937c509
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-04 22:30:31 +0700

    create index method for showing all post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,5 +9,12 @@ use App\Models\Post; // import model Post
 
 class PostController extends Controller
 {
-    //
+    // Method untuk menampilkan semua data Post
+    public function index()
+    {
+        // Mengambil semua data Post
+        $posts = Post::latest()->paginate(5);
+        // Mengembalikan data Post dalam bentuk Resource
+        return new PostResource(true, 'List Data Posts', $posts);
+    }
 }
