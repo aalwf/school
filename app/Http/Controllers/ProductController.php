a8b97be50a55e83787d87ff5f079822a53329195
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-04 10:32:23 +0700

    create method for edit product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse; // RedirectResponse dari Laravel
+use Illuminate\Support\Facades\Storage; // Import Storage dari laravel
 use Illuminate\Http\Request;
 use Illuminate\View\View; // View dari Laravel
 use App\Models\Product; // Model Product
@@ -90,5 +91,53 @@ class ProductController extends Controller
 
         // menampilkan halaman edit product
         return view('pages.edit', $data);
+    }
+
+    // Method untuk mengupdate product
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        // validasi data yang dikirim dari form
+        $request->validate([
+            'image'         => 'image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        // mengambil data product berdasarkan id
+        $product = Product::findOrFail($id);
+
+        // melakukan pengecekan apakah ada file gambar yang dikirim
+        if ($request->hasFile('image')) {
+            // mengambil file gambar
+            $image = $request->file('image');
+
+            // menggunakan method storeAs untuk menyimpan gambar ke folder posts
+            $image->storeAs('public/products', $image->hashName());
+
+            // hapus file gambar lama
+            Storage::delete('public/products/' . $product->image);
+
+            // update data product
+            $product->update([
+                'image'         => $image->hashName(),
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+        } else {
+            // update data product tanpa mengubah gambar
+            $product->update([
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+        }
+
+        // mengalihkan ke halaman posts dengan mengirimkan pesan sukses
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasil Diupdate!']);
     }
 }
