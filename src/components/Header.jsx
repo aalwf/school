a2a7fd53d8bd1a6b0db40b33f3a55d526034c50c
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-05 18:14:27 +0700

    create a function to add todo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,25 @@ const Header = () => {
     // State untuk input
     const [input, setInput] = useState("");
 
+    // Fungsi untuk menambahkan todo
+    const addTodo = () => {
+        // Menyiapkan data baru
+        const newTodo = { input, done: false };
+
+        // Memanggil API untuk menambahkan todo
+        fetch("http://localhost:8000/todos", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newTodo),
+        }).then(() => {
+            // ketika sukses menambah data, reset form dengan mengeset state input menjadi empty string
+            setInput("");
+            console.log("new todo added.");
+        });
+    };
+
     // Tampilan Header
     return (
         <div id="todo-header" className="header">
