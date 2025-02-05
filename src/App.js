214d53761cda67cc391105a7a46a9f25be584983
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-05 15:35:17 +0700

    use a component in App.jsx

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 
-function App() {}
+function App() {
+    <div className="App">
+        <div className="content">
+            <Header /> {/* Memanggil komponen Header */}
+            <TodoList /> {/* Memanggil komponen TodoList */}
+        </div>
+    </div>;
+}
 
 export default App;
