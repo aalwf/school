e9f69f2f471f520d41d37f17f9363a9120e6ef97
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-05 19:17:09 +0700

    integrate updateTodo function

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -19,7 +19,8 @@ const TodoItem = ({ todo }) => {
     return (
         // Menampilkan todo dari props yang dikirim oleh komponen TodoList
         <li className={`${todo.done ? "checked" : ""}`}>
-            {todo.title} <span className="close">x</span>
+            <div onClick={updateTodo}>{todo.title}</div>
+            <span className="close">x</span>
         </li>
     );
 };
