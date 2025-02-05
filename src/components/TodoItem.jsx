fdd44501f50cd325b8646b160584115770f892a2
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-05 15:21:41 +0700

    create Item component

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,10 @@
+// Komponen TodoItem
+const TodoItem = () => {
+    return (
+        <li>
+            Hit the gym <span class="close">x</span>
+        </li>
+    );
+};
+
+export default TodoItem;
