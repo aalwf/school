84a68e371219b839ecd7022ced75aea0a154544a
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-05 18:03:12 +0700

    memanggil API menggunakan useEffect

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,6 +6,25 @@ const TodoList = () => {
     // State untuk todos
     const [todos, setTodos] = useState([]);
 
+    // Menyiapkan useEffect untuk mengambil data todos pada saat komponen dimuat
+    useEffect(() => {
+        // Memanggil API untuk mengambil data todos
+        fetch("http://localhost:8000/todos")
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                // ketika Rest API sukses, simpan data dari response ke dalam state lokal
+                setTodos(data);
+            })
+            .catch((err) => {
+                // ketika Rest API gagal, tampilkan pesan kesalahan
+                if (err.name === "AbortError") {
+                    console.log("fetch aborted.");
+                }
+            });
+    }, []);
+
     return (
         <ul id="todo-list">
             {/* Memanggil komponen TodoItem */}
