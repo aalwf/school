d3efb812bd1dfc354f64c5897db5938945a07a23
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-05 18:31:53 +0700

    mengubah index.js menggunakan root.render() untuk penyesuaian versi dengan react

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
     <React.StrictMode>
-        <App /> {/* Memanggil component App */}
-    </React.StrictMode>,
-
-    document.getElementById("root")
+        <App />
+    </React.StrictMode>
 );
