e9906b4b75e21c8c56343896293c55464a072bde
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-05 15:36:29 +0700

    setup state in TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,11 @@
+import { useEffect, useState } from "react"; // import useEffect dan useState
 import TodoItem from "./TodoItem"; // import komponen TodoItem
 
 // Komponen List
 const TodoList = () => {
+    // State untuk todos
+    const [todos, setTodos] = useState([]);
+
     return (
         <ul id="todo-list">
             {/* Memanggil komponen TodoItem */}
