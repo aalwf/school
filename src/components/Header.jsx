38004b957f3a3d897a558530dba9b9cacedf92f5
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-05 18:09:13 +0700

    create input on Header component to add todo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 // Komponen Header
 const Header = () => {
+    // Tampilan Header
     return (
-        // Tampilan Header
         <div id="todo-header" className="header">
             <h2>Simple Todo App</h2>
-            <input type="text" />
-            <span className="add-button">Add</span>
+            <input type="text" value={""} onChange={""} />
+            <span className="add-button" onClick={""}>
+                Add
+            </span>
         </div>
     );
 };
