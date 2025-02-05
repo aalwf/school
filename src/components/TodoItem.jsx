6fea58ba81680ac1421bb45f27ab2e92d3b8d528
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-05 19:16:03 +0700

    add function to change todo status

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,21 @@
 // Komponen TodoItem
 const TodoItem = ({ todo }) => {
+    // Fungsi untuk mengubah status todo
+    const updateTodo = () => {
+        todo.done = !todo.done;
+
+        fetch("http://localhost:8000/todos/" + todo.id, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(todo),
+        }).then(() => {
+            console.log("todo updated.");
+            setRefresh(true);
+        });
+    };
+
     return (
         // Menampilkan todo dari props yang dikirim oleh komponen TodoList
         <li className={`${todo.done ? "checked" : ""}`}>
