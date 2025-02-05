055ed8094935142f993fdccf73d49fe0158925ee
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-05 19:09:12 +0700

    fix error in index.js & npm packages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>
+        <App /> {/* Memanggil component App */}
+    </React.StrictMode>,
+
+    document.getElementById("root")
 );
