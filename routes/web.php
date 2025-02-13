adb54fb18f5d3c9b69ebcd6bb09da9bf89f8e332
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-13 23:41:33 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * @path /users
+ * @method GET
+ */
+Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
