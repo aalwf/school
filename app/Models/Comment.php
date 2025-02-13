2d891a93e37c22bf6d1a717cde145de1231db180
aalwf <2223594.elgin@smkn-2sbg.sch.id>
2025-02-14 00:00:32 +0700

    create an inverse relationships from the Comment model to the Post

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    /**
+     * post
+     *
+     * @return void
+     */
+    public function post()
+    {
+        // Inverse relationship
+        return $this->belongsTo(Post::class);
+    }
 }
